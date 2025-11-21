d54921afe4bee4f852a79cdf69d5ac5d11503787
Rebeca Pavon Fernandez <rebecapf63@educastur.es>
2025-11-21 17:49:35 +0100

    Initial commit

---
+++ src/test/java/com/example/ExamenMocRPF/ExamenMocRpfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ExamenMocRPF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenMocRpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
